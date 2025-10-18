5d6e7f6e225a077729c834b54e6dd80957b52813
shriatluri <atluri5@purdue.edu>
2025-10-17 22:19:36 -0400

    change

--- backend/index.py
+++
@@ -1,9 +0,0 @@
-"""
-Vercel serverless function entry point
-"""
-from mangum import Mangum
-from app import app
-
-# Wrap FastAPI app with Mangum for Vercel serverless compatibility
-handler = Mangum(app, lifespan="off")
-
